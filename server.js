4578ea99fe5cb90eb3558f030116ddcb3dc1cf37
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-10 23:04:26 +0530

    update server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const conn_url =
   "mongodb+srv://admin:z4BSpx2vy5B86RJU@cluster0.zxswm0a.mongodb.net/?retryWrites=true&w=majority";
 // -----------------
 
+try {
+  await mongoose.connect(conn_url);
+} catch (error) {
+  handleError(error);
+}
+
 app.listen(PORT, () => {
   console.log("Server Running");
 });
