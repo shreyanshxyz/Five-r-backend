e0fea5a8b0a8a7588250413f718400b007de510b
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-10 22:36:46 +0530

    update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 // temporary
 const PORT = 8800;
+const conn_url =
+  "mongodb+srv://admin:z4BSpx2vy5B86RJU@cluster0.zxswm0a.mongodb.net/?retryWrites=true&w=majority";
 // -----------------
 
 app.listen(PORT, () => {
