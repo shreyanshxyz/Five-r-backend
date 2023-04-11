9e10aa90e17cb0fcd9f1ed0b45fb4ad4c19b20cd
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-12 00:04:16 +0530

    update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 const app = express();
-import userRoute from "./routes/user.route";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -17,7 +17,7 @@ const connect = async () => {
 
 app.use("/api/users", userRoute);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   connect();
   console.log("Server Running");
 });
