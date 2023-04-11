fe9f013a9ff1229f920f380044b2b1c2ef68de4b
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-12 02:38:42 +0530

    update user.controller.js and user.route.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,11 +2,15 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const deleteUser = async (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).send("You are not Authenticated");
-  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
-    res.send(payload);
+  const user = await User.findById(req.params.id);
+
+  jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+    if (payload.id !== user._id.toString()) {
+      return res.status(403).send("You can delete only your account");
+    }
+
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send("User Deleted");
   });
-  //   await User.findByIdAndDelete(req.params.id);
 };
 export const getUser = () => {};
