176d928741f87977348bf7b6474e88820fecfd57
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-12 00:05:00 +0530

    update user.route.js and server.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { deleteUser, getUser } from "../controllers/user.controller.js";
-import { verifyToken } from "../middleware/jwt.js";
+// import { deleteUser, getUser } from "../controllers/user.controller.js";
+// import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-routter.get("/test", (req, res) => {
+router.get("/test", (req, res) => {
   res.send("It works");
 });
-router.delete("/:id", verifyToken, deleteUser);
-router.get("/:id", getUser);
+// router.delete("/:id", verifyToken, deleteUser);
+// router.get("/:id", getUser);
 
 export default router;
