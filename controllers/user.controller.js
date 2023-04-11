88269690fd1351822674b2c539d27e6b246859e9
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-12 01:27:58 +0530

    update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,8 @@
 import User from "../models/user.model.js";
 
 export const deleteUser = async (req, res) => {
-  //   const token = req.cookie.accessToken;
-  //   if (!token) return res.status(401).send("You are not Authenticated");
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).send("You are not Authenticated");
   //   jwt.verify(process.env.JWT_KEY, (err, payload) => [res.send(payload)]);
   //   await User.findByIdAndDelete(req.params.id);
 };
