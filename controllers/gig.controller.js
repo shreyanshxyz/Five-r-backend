b2a0888ae80fde5fd52bd0d543343dc802dd8a6f
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-04-16 00:24:39 +0530

    update gig.controller.js

--- controllers/gig.controller.js
+++ controllers/gig.controller.js
@@ -57,7 +57,7 @@ export const getGigs = async (req, res, next) => {
     ...(q.search && { title: { $regex: q.search, $options: "i" } }),
   };
   try {
-    const gigs = await Gig.find(filters).sort({ createdAt: -1 });
+    const gigs = await Gig.find(filters).sort({ [q.sort]: -1 });
     res.status(200).send(gigs);
   } catch (err) {
     next(err);
